fc4dcdcad2bf98554103a5760d08d0eeaf560e64
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-08-16 12:43:03 +0800

    added toggler to editor

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
+
+function EditToggle({title, children}) {
+  const [showForm, setShowForm] = useState(false);
+  let buttonText = showForm? 'CLOSE' : 'EDIT';
+
+  function handleClick() {
+    setShowForm(!showForm);
+  }
+
+  return (
+    <div>
+      <div className="toggler">
+        <p>{title}</p>
+        <button onClick={handleClick}>{buttonText}</button>
+      </div>
+      {showForm && (children)}
+    </div>
+  );
+}
+
 function Editor() {
-    return (
-        <div className="editor">
-            <div>GENERAL INFO</div>
-            <div>EDUCATIONAL EXP</div>
-            <div>PRACTICAL EXP</div>
-        </div>
-    )
+  return (
+    <div className="editor">
+      <EditToggle title='GENERAL INFORMATION'>
+        <form>THIS IS FORM</form>
+      </EditToggle>
+      <EditToggle title='EDUCATIONAL EXPERIENCE'>
+        <form>THIS IS FORM</form>
+      </EditToggle>
+      <EditToggle title='PRACTICAL EXPERIENCE'>
+        <form>THIS IS FORM</form>
+      </EditToggle>
+    </div>
+  );
 }
 
 export default Editor;
